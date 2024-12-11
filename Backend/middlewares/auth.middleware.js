675772d4b9dd824f0eb5d75028e20f59aee30437
AshrafDesai <asharafrazadesai2002@gmail.com>
2024-12-11 10:12:01 +0530

    Initial commit

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,9 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const blackListTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
+
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
     
